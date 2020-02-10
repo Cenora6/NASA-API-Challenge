2bbad3e16a0509c9706da7a95ceea2c2b2c63802
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-02-10 18:17:39 +0100

    Added load more button and scroll top button

--- js/app.js
+++ js/app.js
@@ -4,12 +4,14 @@ $(function () {
     const form = $("form");
     const button = form.find("button");
 
-
-    // const photos = $(".photos");
-    // const gallery = $(".gallery");
-    // gallery.removeChild(photos);
-
+    $('.loader').hide()
     button.on("click", function (e) {
+        const photos = $(".photos");
+        const single = photos.find('.single-photo');
+        const empty = photos.find('.empty');
+        single.remove();
+        empty.remove();
+
         e.preventDefault();
         const input = form.find("input");
         const value = input.val();
@@ -17,27 +19,81 @@ $(function () {
         $.ajax({
             url : 'https://images-api.nasa.gov/search?q=' + value,
             method: 'GET',
-            dataType: "json"
+            dataType: "json",
+            beforeSend: function(){
+                $('.loader').show();
+            },
         }).done(function(result) {
-            showPhotos( result.collection.items );
-            input.val('');
-        });
+            window.setTimeout(function () {
+                if(result.collection.items.length === 0) {
+                    $('.loader').hide();
+                    showEmpty();
+                    input.val('');
+                } else {
+                    $('.loader').hide();
+                    input.val('');
+                    showPhotos( result.collection.items );
+                }
+            }, 1000);
+        })
     });
 
     function showPhotos (photos) {
 
-        for(let i = 0; i < 10; i++){
+        const button = $('.load-more-button').find('button');
+        button.remove();
+
+        const photo = $(".photos");
+        const single = photo.find('.single-photo');
+        const photosNumber = single.length;
+        for(let i = photosNumber; i < photosNumber + 12; i++){
             let imageLinks = photos[i].links[0].href;
             if(imageLinks.indexOf('jpeg') > -1 || imageLinks.indexOf('jpg') > -1) {
-                console.log(imageLinks)
-
-                const photos = $(".photos");
                 const newDiv = $("<div class='single-photo'>");
-                photos.append(newDiv);
+                photo.append(newDiv)
                 const newImg = $('<img alt="photos">');
                 newDiv.append(newImg);
                 newImg.attr('src', imageLinks)
+                newDiv.hide().fadeIn(2000);
             }
         }
+
+        const newDiv = $('<div class="load-more-button">');
+        const newButton = $('<button>Load more</button>');
+        const gallery = $(".gallery");
+        gallery.append(newDiv);
+        newDiv.append(newButton);
+
+        $(".load-more-button").on('click',  function (e) {
+            const button = $('.load-more-button');
+            button.remove();
+            e.preventDefault();
+            showPhotos(photos);
+        });
     }
+
+    function showEmpty () {
+        const photos = $(".photos");
+        const newDiv = $("<div class='empty'> no photos </div>");
+        photos.append(newDiv);
+    }
+
+    const buttonScroll = $('.scroll');
+    if($(window).scrollTop() < 1499){
+        buttonScroll.hide();
+    }
+
+    window.onscroll = function () {
+        const scrollTop = $(window).scrollTop();
+        if ( scrollTop > 1500 ) {
+            buttonScroll.fadeIn();
+        } else {
+            buttonScroll.fadeOut();
+        }
+    };
+
+    buttonScroll.on("click", function () {
+        $('html').animate({scrollTop : 0},1000);
+        return false;
+    })
 });
